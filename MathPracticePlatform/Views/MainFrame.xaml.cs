bc0be07f1c96120ec07bbd5cde40ffc4e61a1511
SnorWeb <adriaenssensniels@hotmail.com>
2025-01-10 13:59:53 +0100

    Created start screen

---
+++ MathPracticePlatform/Views/MainFrame.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathPracticePlatform.Views
+{
+    /// <summary>
+    /// Interaction logic for MainFrame.xaml
+    /// </summary>
+    public partial class MainFrame : Page
+    {
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVermenigvuldigen_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Vermenigvuldigen");
+        }
+
+        private void btnOpties_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Opties");
+        }
+
+        private void btnAfsluiten_Click(object sender, RoutedEventArgs e)
+        {
+           Application.Current.Shutdown();
+        }
+    }
+}
