9ce3458827368aac274df6162a4ad028734f9392
SnorWeb <adriaenssensniels@hotmail.com>
2025-01-11 18:20:52 +0100

    Created View model and add command to the backButton

---
+++ MathPracticePlatform/ViewModels/ExercisePageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using MathPracticePlatform.Services;
+using MathPracticePlatform.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathPracticePlatform.Services;
+using System.Windows.Input;
+
+namespace MathPracticePlatform.ViewModels
+{
+    public class ExercisePageViewModel : BaseViewModel
+    {
+        private string _timer;
+        private string _huidigeOefening;
+        private string _userAntwoord;
+
+        private int _aantalOefeningen;
+        private int _score;
+        private int _fouten;
+
+        public ICommand NavigateBackCommand { get; }
+
+        public ExercisePageViewModel()
+        {
+            NavigateBackCommand = new RelayCommand(GoBack);
+        }
+
+        private void GoBack()
+        {
+            CustomNavigationService.Instance.Navigate(new MainPage());
+        }
+    }
+}
--- MathPracticePlatform/Views/ExercisePage.xaml.cs
+++ MathPracticePlatform/Views/ExercisePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MathPracticePlatform.ViewModels;
 
 namespace MathPracticePlatform.Views
 {
@@ -23,6 +24,7 @@ namespace MathPracticePlatform.Views
         public ExercisePage()
         {
             InitializeComponent();
+            DataContext = new ExercisePageViewModel();
         }
     }
 }
