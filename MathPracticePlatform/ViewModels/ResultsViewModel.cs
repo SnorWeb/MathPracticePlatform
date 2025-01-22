8e1dc8e2783cdb3bf2fb569baa4537ed633ddeb6
SnorWeb <adriaenssensniels@hotmail.com>
2025-01-22 17:08:13 +0100

    Add gif on ResultPage when score is 20

--- MathPracticePlatform/ViewModels/ResultsViewModel.cs
+++ MathPracticePlatform/ViewModels/ResultsViewModel.cs
@@ -16,6 +16,21 @@ namespace MathPracticePlatform.ViewModels
         public string _foutenLijst;
         public string _resterendeTijd;
         public string _title;
+        private string _fout;
+
+        private Uri _gifsource;
+
+        public Uri GifSource
+        {
+            get => _gifsource;
+            set => SetProperty(ref _gifsource, value);
+        }
+
+        public string Fout
+        {
+            get => _fout;
+            set => SetProperty(ref _fout, value);
+        }
 
         public string Title
         {
@@ -75,14 +90,16 @@ namespace MathPracticePlatform.ViewModels
 
             if (FoutenOefeningen != null)
             {
+                Fout = "Fouten: ";
                 foreach (var fout in FoutenOefeningen)
                 {
                     FoutenLijst += fout + "\n";
                 }
             }
-            else
+            if (Score == 20)
             {
-                FoutenLijst = "Geen fouten gemaakt!";
+                Fout = "";
+                GifSource = new Uri("pack://application:,,,/Resources/GIF/fuegos-fired.gif");
             }
         }
 
