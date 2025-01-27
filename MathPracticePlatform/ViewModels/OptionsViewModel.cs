f8b1708dbbd8b71ebe2a8993e1b4b5910474d12a
SnorWeb <adriaenssensniels@hotmail.com>
2025-01-27 10:11:44 +0100

    Created style and space for error message

--- MathPracticePlatform/ViewModels/OptionsViewModel.cs
+++ MathPracticePlatform/ViewModels/OptionsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -11,11 +12,19 @@ namespace MathPracticePlatform.ViewModels
 {
     public class OptionsViewModel : BaseViewModel
     {
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set => _errorMessage = value;
+        }
         public ICommand GoBackCommand { get; }
 
         public OptionsViewModel() 
         {
             GoBackCommand = new RelayCommand(() => NavigateBack());
+            
         }
 
         private void NavigateBack()
