8cafeca09023c8fe7993fbe0b9d789c70c52964a
SnorWeb <adriaenssensniels@hotmail.com>
2025-01-27 11:16:14 +0100

    Add logic for errormessage and created a public parameter where the userinput is going to be saved.

---
+++ MathPracticePlatform/Styles/GlobalState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPracticePlatform.Styles
+{
+    public class GlobalState
+    {
+        private static GlobalState instance;
+        public static GlobalState Instance => instance ?? (instance = new GlobalState());
+        
+        public int TimeLimit { get; set; }
+    }
+}
--- MathPracticePlatform/ViewModels/OptionsViewModel.cs
+++ MathPracticePlatform/ViewModels/OptionsViewModel.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using MathPracticePlatform.Services;
+using MathPracticePlatform.Styles;
 using MathPracticePlatform.Views;
 
 namespace MathPracticePlatform.ViewModels
@@ -13,18 +15,64 @@ namespace MathPracticePlatform.ViewModels
     public class OptionsViewModel : BaseViewModel
     {
         private string _errorMessage;
+        private string _userInput;
+
+        private int _globalTime;
+
+        public string UserInput
+        {
+            get => _userInput;
+            set {
+            _userInput = value;
+            OnPropertyChanged();
+            }
+        }
 
         public string ErrorMessage
         {
             get => _errorMessage;
-            set => _errorMessage = value;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int GlobalTime
+        {
+            get => _globalTime;
+            set
+            {
+                _globalTime = value;
+                OnPropertyChanged();
+            }
         }
         public ICommand GoBackCommand { get; }
+        public ICommand SaveCommand { get; }
 
         public OptionsViewModel() 
         {
+
             GoBackCommand = new RelayCommand(() => NavigateBack());
+            SaveCommand = new RelayCommand(SaveUserInput);
             
+        }
+
+        private void SaveUserInput(object obj)
+        {
+            if (int.TryParse(UserInput, out int parsedTime) && parsedTime >= 30 && parsedTime <= 180)
+            {
+                ErrorMessage = string.Empty;
+                GlobalTime = parsedTime;
+                GlobalState.Instance.TimeLimit = parsedTime;
+                MessageBox.Show($"{parsedTime} is opgeslagen");
+                UserInput = string.Empty;  
+            }
+            else 
+            {
+                ErrorMessage = "Kies een getal tussen de 30 en 180";
+                UserInput = string.Empty;
+            }
         }
 
         private void NavigateBack()
